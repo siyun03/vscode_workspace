1e85068f7fb4744fff324781c9143579cdbd553a
siyun03 <dssiyun@gmail.com>
2025-01-16 17:47:55 +0900

    2025/01/16(2)

--- js/32ex.dom4.js
+++ js/32ex.dom4.js
@@ -1,6 +1,6 @@
-document.getElementById('plus').addEventListener('click', ()=>{
-    // 요소 생성 및 추가
-    const newplus = document.createElement('');
-    newplus.dataset.id = '5';
-    list.appendChild(newplus);
-})
+document.querySelectorAll('#fileList button').forEach(btn => {
+    btn.addEventListener('click', e => {
+        console.log(e.target.dataset.btnId);
+        console.log(e.target.dataset.btnType);
+    });
+});
