d13e3ba82e59678380682957c1693e19f2e9a613
siyun03 <dssiyun@gmail.com>
2025-01-17 17:32:33 +0900

    2025/01/17-

--- js/34ex.event4.js
+++ js/34ex.event4.js
@@ -1,3 +1,9 @@
 // event 실습 4 : 성적처리 (동적 테이블 생성)
 // 데이터 : 성명, 국어, 영어, 수학
 // 기능 : 등록, 삭제, 개인총점, 과목총점
+
+const button = document.getElementById('update').addEventListener('click', e=>{
+
+    
+
+});
